e46c907aa7763a531d8cbbb9750483dfbf0ee19a
damiiegregorio <damiiekeith@gmail.com>
2019-10-25 15:10:36 +0800

    Update gdeck.py

--- gdeck/gdeck.py
+++ gdeck/gdeck.py
@@ -6,9 +6,6 @@ class Card:
         self.suit = suit
         self.rank = rank
 
-    def show_card(self):
-        print("{} of {}".format(self.rank, self.suit))
-
     def __repr__(self):
         return "{} of {}".format(self.rank, self.suit)
 
@@ -23,16 +20,10 @@ class Deck:
 
     def build(self):
         """Merging suit and rank"""
-        return [self.card_list.append(Card(s, v)) for s in self.suit for v in self.rank]
+        return [self.card_list.append(repr(Card(s, v))) for s in self.suit for v in self.rank]
 
     def show_deck(self):
         """Show deck in list form"""
-        return self.card_list
-
-    def show_all(self):
-        """Show all cards"""
-        for c in self.card_list:
-            c.show_card()
         return self.card_list
 
     def choice(self):
@@ -46,17 +37,12 @@ class Deck:
     def __getitem__(self, item):
         return self.card_list[item]
 
-    def __repr__(self):
-        for i in range(len(self.card_list)):
-            return
 
-    def __iter__(self):
-        return self
+deck = Deck()
 
-
-# deck = Deck()
+"""Print commands"""
 # print(deck[0:2])
 # print(deck.choice())
 # print(deck.show_deck())
-# deck.show_all()
-# print(deck.build())
+# print(deck.__len__())
+# print(deck.__getitem__(1))
